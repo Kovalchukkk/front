f14d4944a91b73b80b993e6c61671c3ad9d40ae2
Mykola Kovalchuk <kolyankov@gmail.com>
2023-05-24 19:12:40 +0300

    upload

---
+++ src/config/ApplicationConfig.js
@@ -0,0 +1,5 @@
+//export const ROOT_SERVER = "http://localhost:8080"; 
+export const ROOT_SERVER = "http://192.168.0.119:8080";
+export const ROOT_URI = "/dev";
+
+export const BASE_URL = ROOT_SERVER + ROOT_URI;
